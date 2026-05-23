dd4da5cfdf0579711c87ec2529a4e77f8bcb3d86
DongWook Lee <sh95119@gmail.com>
2026-05-23 13:08:08 +0900

    feat: 스캐너 구현 완료

--- src/Lox.java
+++ src/Lox.java
@@ -4,9 +4,11 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -21,6 +23,8 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -32,6 +36,8 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+
+            hadError = false;
         }
     }
 
@@ -43,5 +49,16 @@ public class Lox {
         for (Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error (int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message
+        );
+        hadError = true;
     }
 }
