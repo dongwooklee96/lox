5e18079fab6772db7288d6e5dfeb6141fd5a4e25
DongWook Lee <sh95119@gmail.com>
2026-06-10 22:34:41 +0900

    feat: 7장 완료

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -27,6 +29,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -53,18 +56,13 @@ public class Lox {
         // 구문 에러 발생 시 멈춘다.
         if (hadError) return;
 
+        interpreter.interpret(expression);
+
         System.out.println(new AstPrinter().print(expression));
     }
 
     static void error (int line, String message) {
         report(line, "", message);
-    }
-
-    static void report(int line, String where, String message) {
-        System.err.println(
-                "[line " + line + "] Error" + where + ": " + message
-        );
-        hadError = true;
     }
 
     static void error(Token token, String message) {
@@ -73,5 +71,17 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
+
+    static void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message
+        );
+        hadError = true;
     }
 }
